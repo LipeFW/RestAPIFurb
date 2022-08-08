38075588874f14ace24aeeb5edacfcb5a0916d20
Felipe Weber <felipe.weber@ambevtech.com.br>
2022-08-08 02:07:30 -0300

    Adapters para objetos e models

---
+++ Models/Adapter/ComandaAdapter.cs
@@ -0,0 +1,18 @@
+﻿using RestAPIFurb.Models.Dto.Comanda;
+
+namespace RestAPIFurb.Models.Adapter
+{
+    public static class ComandaAdapter
+    {
+        public static Comanda FromBody(PostComandaBody body)
+        {
+            return new Comanda
+            {
+                IdUsuario = body.IdUsuario,
+                NomeUsuario = body.NomeUsuario,
+                TelefoneUsuario = body.TelefoneUsuario,
+                Produtos = body.Produtos.Select(ProdutoAdapter.FromBody).ToList()
+            };
+        }
+    }
+}
---
+++ Models/Adapter/ProdutoAdapter.cs
@@ -0,0 +1,17 @@
+﻿using RestAPIFurb.Models.Dto.Comanda;
+
+namespace RestAPIFurb.Models.Adapter
+{
+    public static class ProdutoAdapter
+    {
+        public static Produto FromBody(PostProdutoBody body)
+        {
+            return new Produto
+            {
+                ProdutoId = body.Id,
+                Nome = body.Nome,
+                Preco = body.Preco
+            };
+        }
+    }
+}
