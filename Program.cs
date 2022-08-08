0b45860b7c1eb9ca14a0a6adbb8b034c352691ad
Felipe Weber <felipe.weber@ambevtech.com.br>
2022-08-08 17:08:55 -0300

    Configurações de autenticação

--- Program.cs
+++ Program.cs
@@ -1,19 +1,72 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RestAPIFurb.ApiConfiguration.HeaderConfiguration;
 using RestAPIFurb.Models;
 using RestAPIFurb.Repository;
 using RestAPIFurb.Repository.Interface;
+using RestAPIFurb.Service;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<_DbContext>()
+    .AddDefaultTokenProviders();
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration["Token:Secret"]);
+
+builder.Services.AddSingleton<TokenService>();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = true;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Token:ValidoEm"],
+        ValidIssuer = builder.Configuration["Token:Emissor"],
+    };
+});
+
+
 builder.Services.AddDbContext<_DbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("ApiRESTFurb")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IComandaRepository, ComandaRepository>();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.OperationFilter<AuthorizationHeaderParameterOperationFilter>();
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header. \r\n\r\n Enter the token in the text input below."
+    });
+});
 
 var app = builder.Build();
 
@@ -25,6 +78,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
