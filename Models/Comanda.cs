eb3943c5e11c0499499abaaf5d4a3ccf39926af3
Felipe Weber <felipe.weber@ambevtech.com.br>
2022-08-08 02:08:33 -0300

    Ajustes de entidades

--- Models/Comanda.cs
+++ Models/Comanda.cs
@@ -5,7 +5,7 @@ namespace RestAPIFurb.Models
     public class Comanda
     {
         [Key]
-        public int Id { get; set; }
+        public int ComandaId { get; set; }
         public int IdUsuario { get; set; }
         public string NomeUsuario { get; set; }
         public string TelefoneUsuario { get; set; }
