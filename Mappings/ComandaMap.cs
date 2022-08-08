677c8db0c1938cb9a579fd0f1f60506d51edaab8
Felipe Weber <felipe.weber@ambevtech.com.br>
2022-08-08 02:06:55 -0300

    Criando Mapping de Entidades

---
+++ Mappings/ComandaMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestAPIFurb.Models;
+
+namespace RestAPIFurb.Mappings
+{
+    public class ComandaMap : IEntityTypeConfiguration<Comanda>
+    {
+        public void Configure(EntityTypeBuilder<Comanda> builder)
+        {
+            builder.HasKey(c => c.ComandaId);
+
+            builder.Property(c => c.IdUsuario).IsRequired();
+            builder.Property(c => c.NomeUsuario).HasMaxLength(255).IsRequired();
+            builder.Property(c => c.TelefoneUsuario).HasMaxLength(255).IsRequired();
+
+            builder.HasMany(c => c.Produtos);
+
+            builder.ToTable("Comandas");
+        }
+    }
+}
---
+++ Mappings/ProdutoMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestAPIFurb.Models;
+
+namespace RestAPIFurb.Mappings
+{
+    public class ProdutoMap : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(p => p.ProdutoId);
+
+            builder.Property(p => p.Nome).HasMaxLength(255).IsRequired();
+            builder.Property(p => p.Preco).IsRequired();
+
+            //builder.HasOne(p => p.Comanda).WithMany(p => p.Produtos)
+            //    .HasForeignKey(p => p.ComandaId).IsRequired();
+
+            builder.ToTable("Produtos");
+        }
+    }
+}
