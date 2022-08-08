89a97dddec8fd5c56c292ca256d180217ece5516
Felipe Weber <felipe.weber@ambevtech.com.br>
2022-08-08 17:09:34 -0300

    Criação de service para gerenciamento de token

---
+++ Service/TokenService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RestAPIFurb.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace RestAPIFurb.Service
+{
+    public class TokenService
+    {
+        private IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _configuration["Token:ValidoEm"],
+                Issuer = _configuration["Token:Emissor"],
+                Expires = DateTime.UtcNow.AddMinutes(double.Parse(_configuration["Token:ExpiracaoMinutos"])),
+                SigningCredentials = 
+                new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
